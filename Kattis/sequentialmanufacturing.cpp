6d422ab6632721c2b1e0a9d1f31c1bdd93ac1f12
San-43 <virgosmp.mx@gmail.com>
2025-09-21 18:37:09 -0600

    Add initial implementation of sequential manufacturing solution

---
+++ Kattis/sequentialmanufacturing.cpp
@@ -0,0 +1,57 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+
+#define sz(x) int(x.size())
+#define fi first
+#define se second
+#define pb push_back
+#define edl '\n'
+
+template <typename T>
+void fill_seq(vector<T> &v, T start = 1) {
+    iota(v.begin(), v.end(), start);
+}
+
+constexpr long long LLINF = 2e18;
+constexpr int INF = 2e9;
+constexpr int MOD = 1e9 + 7;
+constexpr int MxN = 2e5 + 5;
+constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+void solve() {
+    int n, p;
+    cin >> n >> p;
+    vi v(n);
+    ll s = 0;
+    ll m = 0;
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+        s += v[i];
+        m = max(m, v[i]);
+    }
+
+    for(int i = 1; i < n; i++) {
+        int k;
+        cin >> k;
+    }
+
+    cout << ((ll) s + (p - 1) * m);
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
