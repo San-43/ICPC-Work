557b7cddb7b58be1b8546cb400a16841c7907a9c
San-43 <virgosmp.mx@gmail.com>
2025-09-29 11:36:00 -0600

    Refactor solve function in B_Friends_and_Subsequences.cpp to implement binary search for range queries and calculate the answer based on limits

--- Sparse Table Codeforces Community Course/B_Friends_and_Subsequences.cpp
+++ Sparse Table Codeforces Community Course/B_Friends_and_Subsequences.cpp
@@ -37,16 +37,63 @@ void solve() {
     }
     for(int i = 0; i < n; i++) {
         cin >> spmax[0][i];
-        if(spmin[0][i] == spmax[0][i]) ans++;
     }
 
-    for(int k = 1; k < 20; k++)
+    vector<int> lg(n + 1, 0);
+    for(int i = 2; i <= n; i++) lg[i] = lg[i >> 1] + 1;
+
+    ll LG = lg[n] + 1;
+
+    for(int k = 1; k < LG; k++)
         for(int i = 0; i + (1 << k) <= n; i++) {
             spmin[k][i] = min(spmin[k - 1][i], spmin[k - 1][i + (1 << (k - 1))]);
             spmax[k][i] = max(spmax[k - 1][i], spmax[k - 1][i + (1 << (k - 1))]);
-            if(spmin[k][i] == spmax[k][i])
-                ans++;
         }
+    
+    auto maxl = [&](int l, int r) {
+        int k = lg[r - l];
+        return max(spmax[k][l], spmax[k][r - (1 << k)]);
+    };
+
+    auto minl = [&](int l, int r) {
+        int k = lg[r - l];
+        return min(spmin[k][l], spmin[k][r - (1 << k)]);
+    };
+
+    auto h = [&](int l, int r) {
+        return maxl(l, r) - minl(l, r);
+    };
+    
+    for(int i = 0; i < n; i++) {
+        int l = i; int r = n - 1;
+
+        int limit1 = -1;
+
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if(h(l, mid) >= 0) {
+                r = mid - 1;
+                limit1 = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        
+        if(h(l, limit1) > 0 || limit1 == -1) continue;
+
+        l = limit1; r = n - 1;
+        int limit2 = n;
+        while(l <= r) {
+            int mid = (l + r) >> 1;
+            if(h(l, mid) > 0) {
+                limit2 = mid;
+                r = mid - 1;
+            } else 
+                l = mid + 1;
+        }
+
+        ans += limit2 - limit1;
+    }
 
     cout << ans << edl;
 }
