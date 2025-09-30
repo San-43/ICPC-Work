558efbcc2353fce53598e2ad883d2da913a0d7b0
San-43 <virgosmp.mx@gmail.com>
2025-09-30 09:42:14 -0600

    Refactor solve function in B_Friends_and_Subsequences.cpp to correct sparse table initialization and improve query handling

--- Sparse Table Codeforces Community Course/B_Friends_and_Subsequences.cpp
+++ Sparse Table Codeforces Community Course/B_Friends_and_Subsequences.cpp
@@ -27,29 +27,30 @@ constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 void solve() {
     int n;
     cin >> n;
-    vector<vector<int>> spmin(20, vector<int>(n + 1));
-    vector<vector<int>> spmax(20, vector<int>(n + 1));
-
-    ll ans = 0;
-
-    for(int i = 0; i < n; i++) {
-        cin >> spmin[0][i];
-    }
-    for(int i = 0; i < n; i++) {
-        cin >> spmax[0][i];
-    }
 
     vector<int> lg(n + 1, 0);
     for(int i = 2; i <= n; i++) lg[i] = lg[i >> 1] + 1;
 
-    ll LG = lg[n] + 1;
+    int LG = lg[n] + 1;
+
+    vector<vector<ll>> spmin(LG, vector<ll>(n));
+    vector<vector<ll>> spmax(LG, vector<ll>(n));
+
+    ll ans = 0;
+
+    for(int i = 0; i < n; i++) {
+        cin >> spmax[0][i];
+    }
+    for(int i = 0; i < n; i++) {
+        cin >> spmin[0][i];
+    }
 
     for(int k = 1; k < LG; k++)
         for(int i = 0; i + (1 << k) <= n; i++) {
             spmin[k][i] = min(spmin[k - 1][i], spmin[k - 1][i + (1 << (k - 1))]);
             spmax[k][i] = max(spmax[k - 1][i], spmax[k - 1][i + (1 << (k - 1))]);
         }
-    
+
     auto maxl = [&](int l, int r) {
         int k = lg[r - l];
         return max(spmax[k][l], spmax[k][r - (1 << k)]);
@@ -63,7 +64,7 @@ void solve() {
     auto h = [&](int l, int r) {
         return maxl(l, r) - minl(l, r);
     };
-    
+
     for(int i = 0; i < n; i++) {
         int l = i; int r = n - 1;
 
@@ -71,28 +72,28 @@ void solve() {
 
         while (l <= r) {
             int mid = (l + r) >> 1;
-            if(h(l, mid) >= 0) {
+            if(h(i, mid + 1) >= 0) {
                 r = mid - 1;
                 limit1 = mid;
             } else {
                 l = mid + 1;
             }
         }
-        
-        if(h(l, limit1) > 0 || limit1 == -1) continue;
+
+        if(limit1 == -1 || h(i, limit1 + 1) != 0) continue;
 
         l = limit1; r = n - 1;
         int limit2 = n;
         while(l <= r) {
             int mid = (l + r) >> 1;
-            if(h(l, mid) > 0) {
+            if(h(i, mid + 1) > 0) {
                 limit2 = mid;
                 r = mid - 1;
-            } else 
+            } else
                 l = mid + 1;
         }
 
-        ans += limit2 - limit1;
+        ans += static_cast<long long>(limit2) - limit1;
     }
 
     cout << ans << edl;
