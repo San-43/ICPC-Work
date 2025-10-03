b57004ae16b1d590b97ad8e764c9293459586487
Sandro Martínez <virgosmp.mx@gmail.com>
2025-10-03 17:57:35 -0600

    Add initial implementation of M_MEDAA_Farouk_and_Bald.cpp with solve function for counting zeros

---
+++ Codeforces/M_MEDAA_Farouk_and_Bald.cpp
@@ -0,0 +1,48 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+
+#define sz(x) int(x.size())
+#define fi first
+#define se second
+#define pb push_back
+#define edl '\n'
+
+constexpr long long LLINF = 2e18;
+constexpr int INF = 2e9;
+constexpr int MOD = 1e9 + 7;
+constexpr int MxN = 2e5 + 5;
+constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    ll ans = 1;
+    for(auto c : s) {
+        if(c == '0') {
+            ans = (ans * 2) % MOD;
+        } 
+    }
+
+    ans = (ans - 1 + MOD) % MOD;
+    cout << ans << edl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
