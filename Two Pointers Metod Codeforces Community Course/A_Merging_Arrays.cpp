e6cae34661bf04f237e80b1a8de4419fb472b1cb
Sandro Martínez <virgosmp.mx@gmail.com>
2025-10-10 23:09:45 -0600

    two pointers

---
+++ Two Pointers Metod Codeforces Community Course/A_Merging_Arrays.cpp
@@ -0,0 +1,56 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+
+#define sz(x) int(x.size())
+#define fi first
+#define se second
+#define pb push_back
+#define edl '\n'
+
+constexpr long long LLINF = 2e18;
+constexpr int INF = 2e9;
+constexpr int MOD = 1e9 + 7;
+constexpr int MxN = 2e5 + 5;
+constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+void solve() {
+    int n, m;
+    cin >> n >> m; 
+    vi v(n);
+    vi a(m);
+    for(auto &e : v) cin >> e;
+    for(auto &e : a) cin >> e;
+
+    int i = 0;
+    int j = 0;
+    vi ans;
+    while(i < n || j < m) {
+        if(j == m || i < n && v[i] < a[j]) {
+            ans.push_back(v[i]);
+            i++;
+        } else {
+            ans.push_back(a[j]);
+            j++;
+        }
+    }
+
+    for(auto &e : ans) cout << e <<" ";
+    cout << edl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
