1a08f23b95ed5acfbaff18f1b843d7a1aa439f03
San-43 <virgosmp.mx@gmail.com>
2025-12-08 12:54:42 -0600

    e

---
+++ Binary Search Codeforces Community Course/E_Equation.cpp
@@ -0,0 +1,55 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+
+#define sz(x) int(x.size())
+#define fi first
+#define se second
+#define pb push_back
+#define edl '\n'
+
+template <typename T>
+void fill_seq(vector<T> &v, T start = 1) {
+    iota(v.begin(), v.end(), start);
+}
+
+constexpr long long LLINF = 2e18;
+constexpr int INF = 2e9;
+constexpr int MOD = 1e9 + 7;
+constexpr int MxN = 2e5 + 5;
+constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+void solve() {
+    double c;
+    cin >> c;
+
+
+    auto ok = [&](double x) {
+        double tmp = x*x + sqrt(x);
+        return tmp >= c;
+    };
+
+    double l = 0.0, r = 1e6;
+    for(int i = 0; i < 60; i++) {
+        double mid = l + (r - l) / 2;
+        if(ok(mid)) r = mid;
+        else l = mid;
+    }
+    cout << setprecision(20) << l << edl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
