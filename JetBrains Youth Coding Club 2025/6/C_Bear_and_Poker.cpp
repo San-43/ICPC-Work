6db581c0e6c0bf5a57708685d2dbcda76224b48f
San-43 <virgosmp.mx@gmail.com>
2025-12-11 01:15:32 -0600

    6

---
+++ JetBrains Youth Coding Club 2025/6/C_Bear_and_Poker.cpp
@@ -0,0 +1,55 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+
+#define sz(x) int(x.size())
+#define fi first
+#define se second
+#define pb push_back
+#define edl '\n'
+
+template <typename T>
+void fill_seq(vector<T> &v, T start = 1) {
+    iota(v.begin(), v.end(), start);
+}
+
+constexpr long long LLINF = 2e18;
+constexpr int INF = 2e9;
+constexpr int MOD = 1e9 + 7;
+constexpr int MxN = 2e5 + 5;
+constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+        while(a[i] % 2 == 0) a[i] /= 2;
+        while(a[i] % 3 == 0) a[i] /= 3;
+    }
+
+    for(int i = 1; i < n; i++) {
+        if(a[i] != a[0]) {
+            cout << "NO" << edl;
+            return;
+        }
+    }
+    cout << "Yes" << edl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
