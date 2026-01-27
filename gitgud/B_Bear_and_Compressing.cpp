2210102c9a8f0d0b6234d1040bc20b87d054ca5b
San-43 <virgosmp.mx@gmail.com>
2026-01-27 11:52:39 -0600

    Add solutions for "B. Bear and Compressing" and "C. Min-Max Sort"

---
+++ gitgud/B_Bear_and_Compressing.cpp
@@ -0,0 +1,68 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+
+#define sz(x) int(x.size())
+#define fi first
+#define se second
+#define pb push_back
+#define edl '\n'
+
+template <typename T>
+void fill_seq(vector<T> &v, T start = 1) {
+    iota(v.begin(), v.end(), start);
+}
+
+constexpr long long LLINF = 2e18;
+constexpr int INF = 2e9;
+constexpr int MOD = 1e9 + 7;
+constexpr int MxN = 2e5 + 5;
+constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+map<string, string> mp;
+ll ans;
+int n, q;
+
+void dfs(string s) {
+    if (s.length() == n) {
+        while (s.length() > 1) {
+            string tmp = s.substr(0, 2);
+            if (!mp.count(tmp)) return;
+            s = mp[tmp] + s.substr(2);
+        }
+        if (s[0] == 'a') ans++;
+        return;
+    }
+    for (char c = 'a'; c <= 'f'; c++) {
+        s += c;
+        dfs(s);
+        s.pop_back();
+    }
+}
+
+void solve() {
+    cin >> n >> q;
+    while (q--) {
+        string a, b;
+        cin >> a >> b;
+        mp[a] = b;
+    }
+    dfs("");
+    cout << ans << edl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
