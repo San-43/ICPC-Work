1abb1e39a9e0ad613145435238c484c20461ffa1
San-43 <virgosmp.mx@gmail.com>
2026-01-30 18:09:17 -0600

    Add solution for "B. Codeforces Subsequences"

---
+++ gitgud/B_Codeforces_Subsequences.cpp
@@ -0,0 +1,56 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+
+#define sz(x) int(x.size())
+#define fi first
+#define se second
+#define pb push_back
+#define edl '\n'
+
+constexpr long long LLINF = 2e18;
+constexpr int INF = 2e9;
+constexpr int MOD = 1e9 + 7;
+constexpr int MxN = 2e5 + 5;
+constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+constexpr int dxx[8] = { 1,  1,  0, -1, -1, -1,  0,  1};
+constexpr int dyy[8] = { 0,  1,  1,  1,  0, -1, -1, -1};
+
+
+void solve() {
+    ll k;
+    cin >> k;
+
+    const string s = "codeforces";
+    const int n = sz(s);
+    vector a(n, 1);
+
+    ll ans = 1;
+    ll l = 0;
+    while (ans < k) {
+        ans = ans / a[l % n] * ++a[l % n];
+        l++;
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < a[i]; j++) {
+            cout << s[i];
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
