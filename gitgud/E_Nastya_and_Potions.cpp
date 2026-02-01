6971a300237c2f183b9b3e0a35e0ad7cebd26dc7
San-43 <virgosmp.mx@gmail.com>
2026-02-01 00:56:50 -0600

    dfs + dp

--- gitgud/E_Nastya_and_Potions.cpp
+++ gitgud/E_Nastya_and_Potions.cpp
@@ -24,9 +24,53 @@ constexpr int MOD = 1e9 + 7;
 constexpr int MxN = 2e5 + 5;
 constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 
+vi ans(MxN);
+vector g(MxN, vector<int>());
+
+ll dfs(int v, vector<ll> &a) {
+    if(ans[v] != -1) return ans[v];
+    if(g[v].empty()) return a[v];
+
+    ll s = 0;
+    for(int i = 0; i < sz(g[v]); i++) {
+        s += dfs(g[v][i], a);
+    }
+
+    return ans[v] = min(a[v], s);
+}
+
+
 void solve() {
-    int n;
-    cin >> n;
+    int n, k;
+    cin >> n >> k;
+    vi a(n+5);
+    for(int i = 1; i <= n; i++) {
+        cin >> a[i];
+        ans[i] = -1;
+    }
+
+    for(int i = 1; i <= k; i++) {
+        int x;
+        cin >> x;
+        a[x] = 0;
+    }
+
+    for(int i = 1; i <= n; i++) {
+        int m;
+        cin >> m;
+        g[i].clear();
+        while(m--) {
+            int x;
+            cin >> x;
+            g[i].pb(x);
+        }
+    }
+
+    for(int i = 1; i <= n; i++) {
+        cout << dfs(i, a) << " ";
+    }
+    cout << edl;
+
 }
 
 int main() {
@@ -34,6 +78,7 @@ int main() {
     cin.tie(nullptr);
 
     int t = 1;
+    cin >> t;
     while (t--) {
         solve();
     }
