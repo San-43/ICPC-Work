eaba7482c72fbffcd99cfb8c6658a755f6fd4d6b
San-43 <virgosmp.mx@gmail.com>
2026-02-04 21:55:16 -0600

    Add implementation of D_Array_and_Operations with solve function

---
+++ gitgud/B_Dreamoon_and_WiFi.cpp
@@ -0,0 +1,62 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R> using o_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+
+#define sz(x) int(x.size())
+#define fi first
+#define se second
+#define pb push_back
+#define edl '\n'
+
+constexpr long long LLINF = 2e18;
+constexpr int INF = 2e9;
+constexpr int MOD = 1e9 + 7;
+constexpr int MxN = 2e5 + 5;
+constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+constexpr int dxx[8] = { 1,  1,  0, -1, -1, -1,  0,  1};
+constexpr int dyy[8] = { 0,  1,  1,  1,  0, -1, -1, -1};
+
+
+void solve() {
+    string a, b;
+    cin >> a >> b;
+    
+    int ansp = 0;
+    for(auto e : a) ansp += (e == '+' ? 1 : -1);
+
+    int finp = 0;
+    int count = 0;
+    for(const auto e : b) {
+        if(e == '+' || e == '-') finp += (e == '+' ? 1 : -1);
+        else count++;
+    }
+
+    ll c = 0;
+    for(int i = 0; i < (1 << count); i++) {
+        (2 * __builtin_popcount(i) - count == ansp - finp) ? c++ : 0;
+    }
+    cout << setprecision(15) << fixed << (static_cast<double>(c) / (1 << count)) << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
