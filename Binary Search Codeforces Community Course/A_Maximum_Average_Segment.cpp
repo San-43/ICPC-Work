1c2a0b694afdb7aa1f2169f67a235a9f75e915c6
San-43 <virgosmp.mx@gmail.com>
2026-02-08 21:49:33 -0600

    finally done (using PopOS!)

--- Binary Search Codeforces Community Course/A_Maximum_Average_Segment.cpp
+++ Binary Search Codeforces Community Course/A_Maximum_Average_Segment.cpp
@@ -27,10 +27,44 @@ constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 void solve() {
     int n, d;
     cin >> n >> d;
-    vi a(n + 1);
+    vector<double> a(n + 1);
     for (int i = 1; i <= n; i++) {
         cin >> a[i];
     }
+
+    int ansl = 1, ansr = d;
+    auto ok = [&](double const x) {
+        vector<double> p(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            p[i] = p[i - 1] + (a[i] - x);
+        }
+
+        double mn = 0;
+        int i = 0;
+        for (int r = d; r <= n; r++) {
+            int j = r - d;
+            if (p[j] < mn) {
+                mn = p[j];
+                i = j;
+            }
+            if (p[r] >= mn) {
+                ansl = i + 1;
+                ansr = r;
+                cerr << i+1 << " " << r << edl;
+                return true;
+            }
+        }
+        return false;
+    };
+
+    double l = 0.0, r = 100.0;
+    for (int i = 0; i <= 80; i++) {
+        double m = l + (r - l) / 2;
+        if (ok(m)) l = m;
+        else r = m;
+    }
+    ok(l);
+    cout << ansl << " " << ansr << edl;
 }
 
 int main() {
